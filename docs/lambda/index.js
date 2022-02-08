238718269cee4c36d0ab9f98d743514920272ce8
Lawrence Bethlenfalvy <lbfalvy@protonmail.com>
2022-02-08 16:30:48 +0100

    Quick URL fix

--- docs/lambda/index.js
+++ docs/lambda/index.js
@@ -30043,11 +30043,11 @@
 	const examples = [
 	    [
 	        'Introduction',
-	        '/lambda/final/index.html?log=%5B%22%23+Lambda+calculus+describes+functions+with+substitution.%22%2C%22%23+A+lambda+function+consists+of+a+variable+name+and+a+body.%22%2C%22%5C%5Cx.x+%2B+1%22%2C%22%23+On+evaluation%2C+all+occurences+of+the+name+in+the+body+are+replaced+with+the+argument.%22%2C%22%28%5C%5Cx.x+%2B+1%29+2%22%2C%22%23+For+convenience+we+often+name+these+expressions%2C+however+recursion+isn%27t+allowed.%22%2C%22F%3D%5C%5Cx.1+%2F+x%22%2C%22%23+This+parser+doesn%27t+recognize+any+operators+not+normally+present+in+lambda+calculus%2C+so+surround+names+with+a+space.%22%5D'
+	        '?log=%5B%22%23+Lambda+calculus+describes+functions+with+substitution.%22%2C%22%23+A+lambda+function+consists+of+a+variable+name+and+a+body.%22%2C%22%5C%5Cx.x+%2B+1%22%2C%22%23+On+evaluation%2C+all+occurences+of+the+name+in+the+body+are+replaced+with+the+argument.%22%2C%22%28%5C%5Cx.x+%2B+1%29+2%22%2C%22%23+For+convenience+we+often+name+these+expressions%2C+however+recursion+isn%27t+allowed.%22%2C%22F%3D%5C%5Cx.1+%2F+x%22%2C%22%23+This+parser+doesn%27t+recognize+any+operators+not+normally+present+in+lambda+calculus%2C+so+surround+names+with+a+space.%22%5D'
 	    ],
 	    [
 	        'Boolean logic',
-	        '/lambda/final/index.html?log=%5B%22TRUE%3D%5C%5Cx.%5C%5Cy.x%22%2C%22FALSE%3D%5C%5Cx.%5C%5Cy.y%22%2C%22NOT%3D%5C%5Cx.x+FALSE+TRUE%22%2C%22NOT+TRUE%22%2C%22AND%3D%5C%5Cx.%5C%5Cy.x+y+x%22%2C%22OR%3D%5C%5Cx.%5C%5Cy.x+x+y%22%5D'
+	        '?log=%5B%22TRUE%3D%5C%5Cx.%5C%5Cy.x%22%2C%22FALSE%3D%5C%5Cx.%5C%5Cy.y%22%2C%22NOT%3D%5C%5Cx.x+FALSE+TRUE%22%2C%22NOT+TRUE%22%2C%22AND%3D%5C%5Cx.%5C%5Cy.x+y+x%22%2C%22OR%3D%5C%5Cx.%5C%5Cy.x+x+y%22%5D'
 	    ]
 	];
 	function App() {
