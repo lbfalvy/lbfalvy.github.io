d27e29ccdda67bd60b132198da982b286a820954
Lawrence Bethlenfalvy <lbfalvy@protonmail.com>
2022-03-07 20:31:02 +0100

    Fixes and adjustments

--- src/components/icons/KoFi2.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import styles from './KoFi2.module.scss'
-import KoFiIcon from './ko-fi-icon.svg'
-
-export default function KoFi2(): React.ReactElement {
-    return <KoFiIcon />
-}
