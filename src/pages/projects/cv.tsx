77d5cf8fc439b7ca505f145dc5e911bc1e08744c
Lawrence Bethlenfalvy <lbfalvy@protonmail.com>
2022-10-17 02:10:57 +0100

    Updated CV to point to english version by default

--- src/pages/projects/cv.tsx
+++ src/pages/projects/cv.tsx
@@ -2,7 +2,7 @@ import Project from "./Project";
 
 export default {
     name: 'Résumé',
-    url: `https://www.lbfalvy.com/cv/?lang=en`,
+    url: `https://www.lbfalvy.com/cv/?lang=en&country=uk`,
     tags: ['react', 'app'],
     description: `
         My CV is written in React, so internationalization and other advanced
